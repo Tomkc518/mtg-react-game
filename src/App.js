1af9bd5f11680b3b2f2b461b4b8dabf8c8a93c74
Tomkc518 <32521131+Tomkc518@users.noreply.github.com>
2018-06-10 17:09:06 -0500

    added footer and images and sorting logic

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import Jumbotron from './components/Jumbotron/Jumbotron.js'
 import ImageBody from './components/ImageBody/ImageBody.js'
 import Images from './components/Images/Images.js'
 import images from "./images.json";
+import Footer from './components/Footer/Footer.js'
 
 class App extends Component {
 
   state = {
-    images
+    images,
+    score: 0,
+    topscore: 0,
+    instructions: "Click an Image to Begin!"
   };
+
+  shuffleImages = () => {
+    let newImagesArr = this.state.images
+    .map(image => [Math.random(), image])
+    .sort((imageOne, imageTwo) => imageOne[0] - imageTwo[0])
+    .map(image => image[1]);
+    this.setState({images: newImagesArr});
+  }
 
   render() {
     return (
       <div>
-        <Navbar instructions={"Click an Image to Begin!"} score={0} topscore={0}/>
+        <Navbar instructions={this.state.instructions} score={this.state.score} topscore={this.state.topscore}/>
         <Jumbotron />
         <ImageBody>
           {this.state.images.map(image => (
@@ -24,9 +36,11 @@ class App extends Component {
               key={image.id}
               name={image.name}
               image={image.image}
+              shuffleImages={this.shuffleImages}
             />
           ))}
         </ImageBody>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <div className="footer">
+        <div className="topFooter"></div>
+        <div className="bottomFooter">
+        <span className="footerText align-middle">MTG Memory Game <i className="fab fa-react"></i></span>
+        </div>
+    </div>
+);
+
+export default Footer;
